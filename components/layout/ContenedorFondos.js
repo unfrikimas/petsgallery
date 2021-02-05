f09196a2b1926d25c321a7cd02848bf020bb69f0
Carlos Enrique Herrera <carlos@unfrikimas.com>
2021-02-05 15:56:35 +0100

    aplicando scroll horizontal a los fondos disponibles

--- components/layout/ContenedorFondos.js
+++ components/layout/ContenedorFondos.js
@@ -25,37 +25,125 @@ export const BACKS = {
     idPublico: "petsgallery/backs/back3_gmufkg.jpg",
     idPublicoModificado: "petsgallery:backs:back3_gmufkg.jpg",
   },
+  rosa: {
+    nombre: "rosa",
+    urlLocal: "/images/backs/back4.jpg",
+    archivoConExtension: "back4_a8jt5i.jpg",
+    archivoSinExtension: "back4_a8jt5i",
+    idPublico: "petsgallery/backs/back4_a8jt5i.jpg",
+    idPublicoModificado: "petsgallery:backs:back4_a8jt5i.jpg",
+  },
+  gris: {
+    nombre: "gris",
+    urlLocal: "/images/backs/back5.jpg",
+    archivoConExtension: "back5_n3rsiu.jpg",
+    archivoSinExtension: "back5_n3rsiu",
+    idPublico: "petsgallery/backs/back5_n3rsiu.jpg",
+    idPublicoModificado: "petsgallery:backs:back5_n3rsiu.jpg",
+  },
 };
 
 const ContenedorFondos = (props) => {
   const { nombre, handleBack } = props;
 
   return (
-    <div className="mt-8 h-28">
-      <ul className="flex h-full space-x-2 items-center justify-center">
-        <li className={`rounded-2xl overflow-hidden ${nombre === 'amarillo' ? "p-1 border-4 border-amarillo" : ""}`}>
-          <img
-            src="/images/backs/back1.jpg"
-            className="h-24 w-24 cursor-pointer rounded-2xl"
-            onClick={() => handleBack(BACKS.amarillo)}
-          />
-        </li>
-        <li className={`rounded-2xl overflow-hidden ${nombre === 'azul' ? "p-1 border-4 border-amarillo" : ""}`}>
-          <img
-            src="/images/backs/back2.jpg"
-            className="h-24 w-24 cursor-pointer rounded-2xl"
-            onClick={() => handleBack(BACKS.azul)}
-          />
-        </li>
-        <li className={`rounded-2xl overflow-hidden ${nombre === 'negro' ? "p-1 border-4 border-amarillo" : ""}`}>
-          <img
-            src="/images/backs/back3.jpg"
-            className="h-24 w-24 cursor-pointer rounded-2xl"
-            onClick={() => handleBack(BACKS.negro)}
-          />
-        </li>
-      </ul>
+    <>
+    <div className="mt-6 section-overflow-x">
+      <div className="relative pl-6 py-2 horizontal-frame-list">
+        <figure className={`rounded-2xl ${nombre === 'amarillo' ? "sombra" : ""}`}>
+          <picture>
+            <img
+              src="/images/backs/back1.jpg"
+              onClick={() => handleBack(BACKS.amarillo)}
+            />
+          </picture>
+        </figure>
+        <figure className={`rounded-2xl ${nombre === 'azul' ? "sombra" : ""}`}>
+          <picture>
+            <img
+              src="/images/backs/back2.jpg"
+              onClick={() => handleBack(BACKS.azul)}
+            />
+          </picture>
+        </figure>
+        <figure className={`rounded-2xl ${nombre === 'negro' ? "sombra" : ""}`}>
+          <picture>
+            <img
+              src="/images/backs/back3.jpg"
+              onClick={() => handleBack(BACKS.negro)}
+            />
+          </picture>
+        </figure>
+        <figure className={`rounded-2xl ${nombre === 'rosa' ? "sombra" : ""}`}>
+          <picture>
+            <img
+              src="/images/backs/back4.jpg"
+              onClick={() => handleBack(BACKS.rosa)}
+            />
+          </picture>
+        </figure>
+        <figure className={`rounded-2xl ${nombre === 'gris' ? "sombra" : ""}`}>
+          <picture>
+            <img
+              src="/images/backs/back5.jpg"
+              onClick={() => handleBack(BACKS.gris)}
+            />
+          </picture>
+        </figure>
+      </div>
     </div>
+      <style jsx>
+      {`
+        .section-overflow-x {
+          overflow-x: auto;
+          overscroll-behavior-x: contain;
+        }
+        .horizontal-frame-list {
+          display: grid;
+          grid-auto-flow: column;
+          grid-auto-columns: 10ch;
+          gap: 1rem;
+        }
+        .horizontal-frame-list::after {
+          content: "";
+          width: 1rem;
+          height: 100%;
+          right: -1rem;
+          inline-size: 1rem;
+          block-size: 100%;
+          inset-end: -1rem;
+        }
+        figure {
+          display: grid;
+          cursor: pointer;
+          margin: 0;
+          user-select: none;
+          transition: transform .2s ease-in-out;
+        }
+        figure:hover {
+          transform: scale(1.1);
+        }
+        picture {
+          display: inline-block;
+          inline-size: 10ch;
+          block-size: 10ch;
+          border-radius: 50%;
+        }
+        img {
+          display: block;
+          inline-size: 100%;
+          block-size: 100%;
+          object-fit: cover;
+          border-radius: 16px;
+
+        }
+        .sombra {
+          box-shadow: 0px 4px 0px #18191F;
+          transform: scale(1.1);
+        }
+      `}
+    </style>
+    </>
   );
 };
 
